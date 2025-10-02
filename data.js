e91e5c4c4b78f3e25c6742f2f5a1811ec8bcd11a
RuthlessG-CYBER <pandasoumya604@gmail.com>
2025-10-02 17:14:50 +0530

    WallpaperBackend

--- data.js
+++ data.js
@@ -2,32 +2,32 @@ const data = [
     {
         id: 1,
         url: "https://i.pinimg.com/736x/e5/70/03/e570030dc8283cd63e81dcb1b4bf3811.jpg",
-        description: "Logo"
+        description: "4k Logo"
     },
     {
         id: 2,
         url: "https://i.pinimg.com/736x/a2/76/7c/a2767cbf1040147eedb6a52be00c77b1.jpg",
-        description: "Logo"
+        description: "4k Asthetic Logo"
     },
     {
         id: 3,
         url: "https://i.pinimg.com/736x/26/f0/9c/26f09c11b39701cedd1160f3ab3fb159.jpg",
-        description: "Logo"
+        description: "4k Ashetic Person"
     },
     {
         id: 4,
         url: "https://i.pinimg.com/736x/10/e4/a7/10e4a7c56dc60f28331e1a105c77a5c4.jpg",
-        description: "Logo"
+        description: "Asthetic Image"
     },
     {
         id: 5,
         url: "https://i.pinimg.com/736x/ec/81/d0/ec81d0cb480e156fba6fd697ab2a1fec.jpg",
-        description: "Logo"
+        description: "4k House"
     },
     {
         id: 6,
         url: "https://i.pinimg.com/736x/2c/cb/39/2ccb39ce6b28e402f0615648e882dad9.jpg",
-        description: "Logo"
+        description: "4k Car"
     }
 ]
 
